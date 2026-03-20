24c63f4d06128af9fa42bb153cf3f636809f0fc1
Kerolinpriya03 <kerolinpriya.a.cse@gmail.com>
2026-03-20 23:44:08 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const mailRoutes = require("./routes/mailRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "https://grand-gnome-569046.netlify.app",
+    credentials: true,
+  })
+);
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
